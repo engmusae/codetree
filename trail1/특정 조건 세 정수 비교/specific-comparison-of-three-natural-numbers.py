3cc5778a945bb64474f991515224e7f13a8562d2
engmusae <jes107444@gmail.com>
2026-09-17 17:07:13 +0900

    [Passed] 특정 조건 세 정수 비교 (46ms, 16MB)

---
+++ trail1/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,17 @@
+arr = list(map(int, input(). split()))
+
+a = arr[0]
+b = arr[1]
+c = arr[2]
+
+if a == min(arr):
+    print('1', end=' ')
+
+else:
+    print('0', end=' ')
+
+if a == b and b == c:
+    print('1')
+
+else:
+    print('0')
