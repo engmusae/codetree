14e747a8d75ddbbf5e7648cefef0a169b2725daf
engmusae <jes107444@gmail.com>
2026-09-17 17:42:13 +0900

    [Passed] 장학금 (55ms, 16MB)

---
+++ trail1/장학금/scholarship.py
@@ -0,0 +1,15 @@
+mid, final = map(float, input(). split())
+
+if mid >= 90:
+
+    if final >= 95:
+        print('100000')
+
+    elif final >= 90:
+        print('50000')
+
+    else:
+        print('0')
+
+else:
+    print('0')
