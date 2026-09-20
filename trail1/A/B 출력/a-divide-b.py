eb16b76e64ae6097d000fd86a0ae02b02e8a1af3
engmusae <jes107444@gmail.com>
2026-09-20 20:50:36 +0900

    [Passed] A/B 출력 (50ms, 16MB)

--- trail1/A/B 출력/a-divide-b.py
+++ trail1/A/B 출력/a-divide-b.py
@@ -3,8 +3,8 @@ A, B = map(int, input(). split())
 mok1 = A // B
 print(mok1, end = '.')
 
-left = A % B * 10
+A = A % B * 10
 for n in range(20):
-    mok2 = (left // B)
-    left = (left - (B * mok2)) * 10
+    mok2 = A // B
+    A = (A - (B * mok2)) * 10
     print(mok2, end = '')
