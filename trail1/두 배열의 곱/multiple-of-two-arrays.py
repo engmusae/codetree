9505a12f0a5a3d8bfbfd16703de05a9236f8ee0d
engmusae <jes107444@gmail.com>
2026-09-26 04:32:57 +0900

    [Passed] 두 배열의 곱 (52ms, 16MB)

---
+++ trail1/두 배열의 곱/multiple-of-two-arrays.py
@@ -0,0 +1,10 @@
+arr1 = [list(map(int, input(). split())) for _ in range(3)]
+what = input()
+arr2 = [list(map(int, input(). split())) for _ in range(3)]
+arr = [[0 for _ in range(3)] for _ in range(3)]
+
+for i in range(3):
+    for j in range(3):
+        arr[i][j] = arr1[i][j] * arr2[i][j]
+        print(arr[i][j], end = ' ')
+    print()
