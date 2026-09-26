36183cfc725359776b288a0d63675a995a41cab3
engmusae <jes107444@gmail.com>
2026-09-26 16:15:22 +0900

    [Passed] 배열로 사각형 만들기 (55ms, 16MB)

---
+++ trail1/배열로 사각형 만들기/print-array-in-rectangle.py
@@ -0,0 +1,15 @@
+arr = [[0 for _ in range(5)] for _ in range(5)]
+
+for i in range(5):
+    for j in range(5):
+        if i == 0:
+            arr[i][j] = 1
+        elif j == 0:
+            arr[i][j] = 1
+        else:
+            arr[i][j] = arr[i - 1][j] + arr[i][j - 1]
+
+for i in range(5):
+    for j in range(5):
+        print(arr[i][j], end = ' ')
+    print()
